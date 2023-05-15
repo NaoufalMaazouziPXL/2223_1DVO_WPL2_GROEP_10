a3e63d6d05fecf711432b8382233dba2b79d99b6
Naoufal <43816561+NaoufalMaazouziPXL@users.noreply.github.com>
2023-05-15 14:41:57 +0200

    Index img aangepast zodat we het kunnen targeten met een eventListener.
    Eerst javascript aangemaakt om profile icon aan te passen als we hoveren

---
+++ demo.js
@@ -0,0 +1,9 @@
+let profileIcon = document.getElementById('user-icon');
+
+profileIcon.addEventListener('mouseover', function () {
+    profileIcon.src = 'assets/profile7.png';
+})
+
+profileIcon.addEventListener('mouseout', function () {
+    profileIcon.src = 'assets/icon%20_user_.svg';
+})
